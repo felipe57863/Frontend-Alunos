a72ebeaea232962cb39b1dd69c74b23a75170b30
Luis Felipe <felipe57863@outlook.com>
2026-05-27 18:01:53 -0300

    fix: gera qr code localmente

--- src/app/features/professor/qrcodes/qrcodes.ts
+++ src/app/features/professor/qrcodes/qrcodes.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import * as QRCode from 'qrcode';
@@ -15,7 +15,10 @@ export class QRCodesComponent implements OnInit {
   sessionCode = '';
   qrCodeDataUrl: string | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.sessionCode = this.route.snapshot.queryParamMap.get('hash') || '';
@@ -25,17 +28,22 @@ export class QRCodesComponent implements OnInit {
     }
   }
 
-  async gerarQRCode() {
+  async gerarQRCode(): Promise<void> {
     const linkAluno = `${window.location.origin}/aluno/validar?hash=${this.sessionCode}`;
 
     try {
-      this.qrCodeDataUrl = await QRCode.toDataURL(linkAluno);
+      this.qrCodeDataUrl = await QRCode.toDataURL(linkAluno, {
+        width: 250,
+        margin: 1,
+      });
+
+      this.cdr.detectChanges();
     } catch (error) {
       console.error('Erro ao gerar QR Code:', error);
     }
   }
 
-  copiarLink() {
+  copiarLink(): void {
     const link = `${window.location.origin}/aluno/validar?hash=${this.sessionCode}`;
 
     navigator.clipboard.writeText(link).then(() => {
